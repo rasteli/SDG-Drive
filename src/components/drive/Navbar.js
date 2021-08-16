582223334448f1062ef4fdc3511fc4fb3fb51b3f
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2021-08-16 07:50:35 -0300

    Beautified

--- src/components/drive/Navbar.js
+++ src/components/drive/Navbar.js
@@ -1,11 +1,21 @@
 import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import logo from "../../sdg.svg"
 
 export default function NavbarComponent() {
   return (
     <Navbar bg="light" expand="sm" className="p-3">
-      <Navbar.Brand as={Link} to="/">
+      <Navbar.Brand as={Link} to="/" style={{ marginRight: "100px" }}>
+        <img
+          src={logo}
+          alt=""
+          style={{
+            transform: "scale(.7)",
+            marginLeft: "-15px",
+            marginTop: "-10px"
+          }}
+        />
         SDG Drive
       </Navbar.Brand>
       <Nav>
