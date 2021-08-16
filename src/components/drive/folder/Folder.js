7d7308fb4b3db950fe576d37c35060c938aa29f1
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2021-08-16 09:12:59 -0300

    Inverted bootstrap button

--- src/components/drive/folder/Folder.js
+++ src/components/drive/folder/Folder.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFolder } from "@fortawesome/free-solid-svg-icons"
 
+import "./style/Folder.css"
+
 export default function Folder({ folder }) {
   return (
     <Link
@@ -10,7 +12,7 @@ export default function Folder({ folder }) {
         pathname: `/folder/${folder.id}`,
         state: { folder }
       }}
-      className="btn btn-dark text-truncate w-100"
+      className="btn btn-dark text-truncate w-100 folder"
     >
       <FontAwesomeIcon icon={faFolder} style={{ marginRight: "10" }} />
       {folder.name}
